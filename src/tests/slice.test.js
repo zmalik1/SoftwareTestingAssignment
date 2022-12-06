9dea1323149771723bb76fbd66fbe0b2b3e23020
Eelis Roininen <eelis.roininen@tuni.fi>
2022-12-06 10:15:56 +0200

    Add coveralls script to npm test, update gitignore

--- src/tests/slice.test.js
+++ src/tests/slice.test.js
@@ -20,4 +20,21 @@ describe('slice', () => {
   it('return empty array if start is greater than end', () => {
     expect(slice([1,2,3,4,5], 2, 1)).toBeEmpty()
   })
+
+  it('returns empty array if parameter is null', () => {
+    expect(slice(null, 1, 2)).toBeEmpty()
+  })
+
+  it('returns empty array if end is null', () => {
+    expect(slice([1,2,3], 1, null)).toBeEmpty()
+  })
+
+  it('return empty array if start is null', () => {
+    expect(slice([1,2,3], null)).toBeEmpty()
+  })
+
+  it('takes end as offset from array end if end is negative', () => {
+    expect(slice([1,2,3,4], 0, -1)).toStrictEqual([1,2,3])
+  })
+
 })
