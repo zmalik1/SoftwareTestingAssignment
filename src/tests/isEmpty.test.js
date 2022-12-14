72392a9a64dc434278ee151387fc79adea312510
Zohaib Malik <zohaib.malik@tuni.fi>
2022-12-14 05:52:59 +0200

    isEmpty

--- src/tests/isEmpty.test.js
+++ src/tests/isEmpty.test.js
@@ -1,6 +1,9 @@
 import isEmpty from '../isEmpty'
 
 describe('isEmpty', () => {
+  const mapOne = new Map();
+  const mapTwo= new Map();
+  mapTwo.set('one', 1);
   it('check empty array', () => {
     expect(isEmpty([], () => true)).toBeEmpty
   })
@@ -12,5 +15,20 @@ describe('isEmpty', () => {
   })
   it('check null value', () => {
     expect(isEmpty(null)).toBe(true)
+  })
+  it('check not empty array', () => {
+    expect(isEmpty([1, 2, 3], () => false)).toBeEmpty
+  })
+  it('check not empty object', () => {
+    expect(isEmpty({ 'a': 1 }, () => false)).toBeEmpty
+  })
+  it('check not empty string', () => {
+    expect(isEmpty(('abc'), () => false)).toBeEmpty
+  })
+  it('check empty map', () => {
+    expect(isEmpty((mapOne), () => true)).toBeEmpty
+  })
+  it('check not empty map', () => {
+    expect(isEmpty((mapTwo), () => false)).toBeEmpty
   })
 })
