731b716ec392e17c0d619aa9d2d935bcd4cc89ca
Zohaib Malik <zohaib.malik@tuni.fi>
2022-12-14 10:14:03 +0200

    updated

--- src/tests/get.test.js
+++ src/tests/get.test.js
@@ -1,12 +1,16 @@
 import get from '../get'
 
 describe('get', () => {
-    function objPath(object, path){
-      return path.match().reduce ((r, k) => r[k], object );
-    }
-    const object = { 'a': [{ 'b': { 'c': 3 } }] }
+  function objPath(object, path){
+    return path.match().reduce ((r, k) => r[k], object );
+  }
+  const object = { 'a': [{ 'b': { 'c': 3 } }] }
 
-  it('compare objects', () => {
+  it('path', () => {
     expect(get(objPath(object, 'a[0].b.c')))
+  })
+
+  it('returns default when default given', () => {
+    expect(get(object, 'a.b.c', 'default')).toBe('default')
   })
 })
